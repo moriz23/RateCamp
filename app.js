a4af2407d7743b77ce62437947d0ae6503536fff
Lebrezzy <morizvi07@gmail.com>
2016-10-31 02:38:13 +0000

    changed flash message

--- app.js
+++ app.js
@@ -60,5 +60,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("The RateCamp Server Has Started!");
 });
